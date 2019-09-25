0839f2dd8ef71f03fa5acd7f37279e0589e6af56
parthhartler <parth@hartleylab.com>
2019-09-25 10:52:05 +0530

    boilerplate code

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Main extends React.Component {
+  render() {
+    return <div className="parent"> React-Redux Boiler Plate </div>;
+  }
+}
+
+export default Main;
